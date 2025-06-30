403156508334e6b7f42bd2da02bc02ad30a39012
sirisha2290 <sirishareddy2290@gmail.com>
2025-06-30 23:00:07 +0530

    Add files via upload

---
+++ Challenge1VariableDeclarationandInitialization.java
@@ -0,0 +1,20 @@
+public class Challenge1VariableDeclarationandInitialization {
+    public static void main(String[] args) {
+        int integerdeclaration;
+        String strdeclaration;
+        boolean bdeclaration;
+        float fdeclaration;
+        double ddeclaration;
+        char cdeclaration;
+        integerdeclaration = 10;
+        strdeclaration = "Sirisha";
+        fdeclaration = 5/2;
+        ddeclaration =3.14;
+        cdeclaration = 'S' ;
+        System.out.println("Integer Assigned value " +integerdeclaration );
+        System.out.println("String Assigned value " +strdeclaration );
+        System.out.println("Float Assigned value " +fdeclaration );
+        System.out.println("Double Assigned value " +ddeclaration );
+        System.out.println("Character Assigned value " +cdeclaration );
+    }
+}
