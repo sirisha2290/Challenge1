35c90fb2b580604c7bfb990c5cc884fa0ede0e46
sirisha2290 <sirishareddy2290@gmail.com>
2025-06-30 23:05:59 +0530

    Add files via upload

---
+++ Challenge1ArithmeticandAssignmentOperators.java
@@ -0,0 +1,11 @@
+public class Challenge1ArithmeticandAssignmentOperators {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 5;
+        System.out.println("Addition: " + (a+b));
+        System.out.println("Substraction: " + (a-b));
+        System.out.println("Multiplication: " + (a*b));
+        System.out.println("Division: " + (a/b));
+        System.out.println("Modulus: " + (a|b));
+    }
+}
